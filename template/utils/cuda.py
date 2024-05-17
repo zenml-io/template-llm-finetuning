26f1cca0d7157e0fe3294194e8e423a1570bc0d1
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2024-05-17 13:02:10 +0200

    add logging

--- template/utils/cuda.py
+++ template/utils/cuda.py
@@ -3,9 +3,13 @@
 import gc
 
 import torch
+from zenml.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 def cleanup_memory() -> None:
     """Clean up GPU memory."""
+    logger.info("Cleaning up GPU memory on the machine...")
     while gc.collect():
         torch.cuda.empty_cache()
